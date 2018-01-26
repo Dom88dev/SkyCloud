647b9b58f540e08de10044267a785728ef40aa5e
hj <ttlle12@naver.com>
2018-01-26 17:07:58 +0900

    출결 관련 dao추가 및 회원 출석달력 수정

--- SkyCloud/src/controller/AjaxController.java
+++ SkyCloud/src/controller/AjaxController.java
@@ -2,12 +2,20 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import bean.AttendanceDao;
+import model.Attendance;
+import model.StudyTimePlace;
 
 
 /**
@@ -23,9 +31,26 @@ public class AjaxController extends HttpServlet {
 		response.setHeader("Cache-Control", "no-cache");
 		PrintWriter out = response.getWriter();
 		String command = request.getParameter("command");
+		
+		AttendanceDao attendanceDao;
+		Attendance att;
+		
 		switch(command) {
 		case "VALIDITYTEST_REGISTER":
 			response.setContentType("text/plain");
+			break;
+			
+		case "CHECKATT": //출석버튼 눌렀을때 작업
+			int att1 = Integer.parseInt((String) request.getSession().getAttribute("mystd_id"));
+			attendanceDao = new AttendanceDao();
+			String status=null;
+			try {
+				status = attendanceDao.InsertAttStatus(att1);
+				
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			out.println(status);
 			break;
 		}
 	}
